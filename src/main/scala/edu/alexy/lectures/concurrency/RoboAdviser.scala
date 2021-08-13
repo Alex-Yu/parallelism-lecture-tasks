4ebb5bfd8daf01e9ae096dbab37b77f0b651035b
maksymvoznyi <maksym.voznyi@intellias.com>
2021-08-13 15:27:59 +0300

    fixes

--- src/main/scala/edu/alexy/lectures/concurrency/RoboAdviser.scala
+++ src/main/scala/edu/alexy/lectures/concurrency/RoboAdviser.scala
@@ -17,10 +17,8 @@ object RoboAdviser {
       }
 
   private def futureRetry[T](block: => Future[T], retries: Int): Future[T] = {
-    val result: Future[T] = block
-
-    result.recoverWith {
-      case _ if retries > 1 => futureRetry[T](block, retries - 1)
+    block.recoverWith {
+      case _ if retries > 0 => futureRetry[T](block, retries - 1)
     }
   }
 
@@ -30,8 +28,8 @@ object RoboAdviser {
   // Possible error should be returned as a ServiceError.
   def getAllTickersRetryable(retries: Int = 10): Future[Seq[String]] =
     futureRetry(Db.getAllTickers, retries).transform {
-      case Failure(exception) => Failure(DbError)
-      case other              => other
+      case Failure(_) => Failure(DbError)
+      case other      => other
     }
 
   // Task 3.
@@ -41,8 +39,8 @@ object RoboAdviser {
   def getCompanyRetryable(ticker: String,
                           retries: Int = 10): Future[Option[Company]] =
     futureRetry(Db.getCompanyLastFinancials(ticker), retries).transform {
-      case Failure(exception) => Failure(DbError)
-      case other              => other
+      case Failure(_) => Failure(DbError)
+      case other      => other
     }
 
   // Task 4.
@@ -51,66 +49,52 @@ object RoboAdviser {
   // Possible error should be returned as a ServiceError.
   def getPriceRetryable(ticker: String, retries: Int = 10): Future[Double] =
     futureRetry(WebApi.getPrice(ticker), retries).transform {
-      case Failure(exception) => Failure(WebApiError)
-      case other              => other
+      case Failure(_) => Failure(WebApiError)
+      case other      => other
     }
 
   // Task 5.
   // Using retryable functions return all tickers with their real time prices.
   def getAllTickersPrices: Future[Seq[(String, Double)]] =
-    for (tickers <- getAllTickersRetryable();
-         tikerWithPrice <- Future.traverse(tickers) { ticker =>
-           Future.successful(ticker).zip(getPriceRetryable(ticker))
-         }) yield tikerWithPrice
+    for {
+      tickers <- getAllTickersRetryable()
+      tickerWithPrice <- Future.traverse(tickers)(ticker =>
+        getPriceRetryable(ticker).map(ticker -> _))
+    } yield tickerWithPrice
 
   // Task 6.
   // Using `getCompanyRetryable` and `getPriceRetryable` functions return a company with its real time stock price.
-  def getCompanyFinancialsWithPrice(ticker: String): Future[(Company, Double)] = {
-    getCompanyRetryable(ticker).collect {
-      case Some(company) =>
-        Future.successful(company).zip(getPriceRetryable(company.ticker))
+  def getCompanyFinancialsWithPrice(ticker: String): Future[(Company, Double)] =
+    getCompanyRetryable(ticker).zip(getPriceRetryable(ticker)).collect {
+      case (Some(company), price) => company -> price
     }
-  }.flatten
 
   // Task 7.
   // Implement a function that returns a list of chip ('Company.isCheap') companies
   // with their real time stock prices using 'getAllTickersRetryable' and
   // 'getCompanyFinancialsWithPrice' functions.
-  def buyList: Future[Seq[(Company, Double)]] = {
-    for (tickers <- getAllTickersRetryable();
-         companyWithPriceSeq <- Future.traverse(tickers) { ticker =>
-           getCompanyFinancialsWithPrice(ticker)
-             .collect {
-               case companyWithPrice @ (company, price)
-                   if company.isCheap(price) =>
-                 Success(companyWithPrice)
-             }
-             .recover { case x => Failure(x) }
-         }) yield {
-      companyWithPriceSeq.collect {
-        case Success(companyWithPrice) => companyWithPrice
+  def buyList: Future[Seq[(Company, Double)]] =
+    for {
+      tickers <- getAllTickersRetryable()
+      companyWithPriceSeq <- Future.traverse(tickers)(
+        getCompanyFinancialsWithPrice)
+    } yield
+      companyWithPriceSeq.filter {
+        case (company, price) => company.isCheap(price)
       }
-    }
-  }
 
   // Task 8.
   // Implement a function that returns a list of expensive ('Company.isExpensive') companies
   // with their real time stock prices using 'getAllTickersRetryable', 'getCompanyRetryable',
   // 'getPriceRetryable' and zipping.
-  def sellList: Future[Seq[(Company, Double)]] = {
-    for (tickers <- getAllTickersRetryable();
-         companyWithPriceSeq <- Future.traverse(tickers) { ticker =>
-           getCompanyRetryable(ticker)
-             .zip(getPriceRetryable(ticker))
-             .collect {
-               case (Some(company), price) if company.isExpensive(price) =>
-                 Success((company, price))
-             }
-             .recover { case x => Failure(x) }
-         }) yield {
+  def sellList: Future[Seq[(Company, Double)]] =
+    for {
+      tickers <- getAllTickersRetryable()
+      companyWithPriceSeq <- Future.traverse(tickers)(ticker =>
+        getCompanyRetryable(ticker).zip(getPriceRetryable(ticker)))
+    } yield
       companyWithPriceSeq.collect {
-        case Success(companyWithPrice) => companyWithPrice
+        case (Some(company), price) if company.isExpensive(price) =>
+          company -> price
       }
-    }
-  }
 }
